8bd317ccef8d06ab3819e49902d74e7eea35b794
uhn <coinhu1995@gmail.com>
2017-09-10 20:16:20 +0700

    tao xong top, header. dang tao menu

--- src/main/java/ptit/nhunh/ui/HeaderUi.java
+++ src/main/java/ptit/nhunh/ui/HeaderUi.java
@@ -7,19 +7,12 @@ import java.util.Date;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @ManagedBean
 @SessionScoped
-public class HelloBean implements Serializable {
+public class HeaderUi implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@Getter
-	@Setter
-	private String name = "nhu";
-	
 	public String getCurrentDate() {
 		SimpleDateFormat sdf = new SimpleDateFormat("EEEEE, MMMMM MM, YYYY");
 		Date date = new Date(System.currentTimeMillis());
